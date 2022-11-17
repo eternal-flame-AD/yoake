b49830aaefeb0bc081a8cdef3e5c6d3b6d6e52c2
eternal-flame-AD <yume@yumechi.jp>
2022-11-16 20:09:07 -0600

    med: add time override

--- webroot/lib/lib.js
+++ webroot/lib/lib.js
@@ -12,3 +12,9 @@ function labelTimeElement(tag, time) {
     tag.setAttribute("data-bs-title", time.format("L LT"));
     new bootstrap.Tooltip(tag);
 }
+
+function dateInZone(zone) {
+    if (zone)
+        return new Date(new Date().toLocaleString("en-US", { timeZone: zone }));
+    return new Date()
+}
